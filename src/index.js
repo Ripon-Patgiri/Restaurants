7e0ca5407b6d5eecd27373429518b8521b52fd66
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-11-21 23:24:35 +0530

    Working Styling for now

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./homeLoad";
 
+// Extracting Buttons into Variables
+let homeBtn = document.getElementById('homeBtn');
+let menuBtn = document.getElementById('menuBtn');
+let contactBtn = document.getElementById('contactBtn');
+
+
+
+
 
