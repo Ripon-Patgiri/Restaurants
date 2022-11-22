237b5c3b8abe05478e6090c5232cd7b4816ecbc7
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-11-22 21:32:25 +0530

    Fixed UI Element Subcontainer.

--- src/homeLoad.js
+++ src/homeLoad.js
@@ -1,13 +1,16 @@
-let content = document.getElementById('content');
+const content = document.getElementById('content');
 
 function displayHome() {
   content.innerHTML = '';
+  const subcontainer = document.createElement('div');
+  subcontainer.classList.add('subcontainer');
   let mainLine = document.createElement("p");
   mainLine.innerText = "Enjoy Traditional Assamese Dish";
   let subLine = document.createElement("p");
   subLine.innerText = "Picture of Stereotypical Assamese Dish";
-  content.appendChild(mainLine);
-  content.appendChild(subLine);
+  subcontainer.appendChild(mainLine);
+  subcontainer.appendChild(subLine);
+  content.appendChild(subcontainer);
 }
 
 export  { displayHome};
