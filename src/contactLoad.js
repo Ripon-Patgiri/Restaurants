61034dc6a4e982f399a0c185d0542debf0cfb29b
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-11-25 21:30:22 +0530

    contactLoad functionality and styling with footer styling.

--- src/contactLoad.js
+++ src/contactLoad.js
@@ -1,3 +1,5 @@
+import mapImag from './map.jpg';
+
 let content = document.getElementById('content');
 let homeBtn = document.getElementById('homeBtn');
 let menuBtn = document.getElementById('menuBtn');
@@ -9,12 +11,21 @@ function displayContact() {
   homeBtn.classList.remove('BtnStyle');
   menuBtn.classList.remove('BtnStyle');
   contactBtn.classList.add('BtnStyle');
+  const subcontainer = document.createElement('div');
+  subcontainer.classList.add('subcontainer');
   let mainLine = document.createElement("p");
-  mainLine.innerText = "Contact Me Here";
+  mainLine.innerText = "Contact Here";
+  mainLine.style.fontSize = '24px';
   let subLine = document.createElement("p");
   subLine.innerText = "7002758935";
-  content.appendChild(mainLine);
-  content.appendChild(subLine);
+  subLine.style.fontSize = '20px';
+  let mapImg = document.createElement('img');
+  mapImg.src = mapImag;
+  mapImg.classList.add('mapImg');
+  subcontainer.appendChild(mapImg);
+  subcontainer.appendChild(mainLine);
+  subcontainer.appendChild(subLine);
+  content.appendChild(subcontainer);
 }
 
 export  { displayContact};
